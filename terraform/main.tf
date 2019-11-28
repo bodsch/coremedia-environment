c70ba3f1a78a5fee5f14cba339329f898589fa7a
Bodo Schulz <bodo@boone-schulz.de>
2019-11-28 06:55:05 +0100

    enable all VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,33 +64,33 @@ resource "libvirt_volume" "cm-monitoring-qcow2" {
   size           = "${lookup(var.server_monitoring, "disk_size", "") == "" ? "0" : var.server_monitoring["disk_size"] }"
 }
 
-# resource "libvirt_volume" "cm-database-qcow2" {
-#   name           = "cm-${var.server_database["hostname"]}.qcow2"
-#   base_volume_id = "${libvirt_volume.centos_image.id}"
-#   pool           = "default"
-#   size           = "${lookup(var.server_database, "disk_size", "") == "" ? "0" : var.server_database["disk_size"] }"
-# }
-#
-# resource "libvirt_volume" "cm-backend-qcow2" {
-#   name           = "cm-${var.server_backend["hostname"]}.qcow2"
-#   base_volume_id = "${libvirt_volume.centos_image.id}"
-#   pool           = "default"
-#   size           = "${var.server_backend["disk_size"]}"
-# }
-#
-# resource "libvirt_volume" "cm-frontend-qcow2" {
-#   name           = "cm-${var.server_frontend["hostname"]}.qcow2"
-#   base_volume_id = "${libvirt_volume.centos_image.id}"
-#   pool           = "default"
-#   size           = "${var.server_frontend["disk_size"]}"
-# }
-#
-# resource "libvirt_volume" "cm-delivery-qcow2" {
-#   name           = "cm-${var.server_delivery["hostname"]}.qcow2"
-#   base_volume_id = "${libvirt_volume.centos_image.id}"
-#   pool           = "default"
-#   size           = "${var.server_delivery["disk_size"]}"
-# }
+resource "libvirt_volume" "cm-database-qcow2" {
+  name           = "cm-${var.server_database["hostname"]}.qcow2"
+  base_volume_id = "${libvirt_volume.centos_image.id}"
+  pool           = "default"
+  size           = "${lookup(var.server_database, "disk_size", "") == "" ? "0" : var.server_database["disk_size"] }"
+}
+
+resource "libvirt_volume" "cm-backend-qcow2" {
+  name           = "cm-${var.server_backend["hostname"]}.qcow2"
+  base_volume_id = "${libvirt_volume.centos_image.id}"
+  pool           = "default"
+  size           = "${var.server_backend["disk_size"]}"
+}
+
+resource "libvirt_volume" "cm-frontend-qcow2" {
+  name           = "cm-${var.server_frontend["hostname"]}.qcow2"
+  base_volume_id = "${libvirt_volume.centos_image.id}"
+  pool           = "default"
+  size           = "${var.server_frontend["disk_size"]}"
+}
+
+resource "libvirt_volume" "cm-delivery-qcow2" {
+  name           = "cm-${var.server_delivery["hostname"]}.qcow2"
+  base_volume_id = "${libvirt_volume.centos_image.id}"
+  pool           = "default"
+  size           = "${var.server_delivery["disk_size"]}"
+}
 
 
 
@@ -138,156 +138,155 @@ resource "libvirt_domain" "cm-monitoring" {
 }
 
 
-# resource "libvirt_domain" "cm-database" {
-#   name   = "${var.server_database["hostname"]}"
-#   memory = "${var.server_database["memory"]}"
-#   vcpu   = "${var.server_database["vcpu"]}"
-#
-#   cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
-#
-#   network_interface {
-#     network_id = "${libvirt_network.vm_network.id}"
-#
-#     hostname  = "${var.server_database["hostname"]}"
-#     addresses = [ "${var.server_database["ip"]}" ]
-#     wait_for_lease = 1
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_port = "0"
-#     target_type = "serial"
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_type = "virtio"
-#     target_port = "1"
-#   }
-#
-#   disk {
-#     volume_id = "${libvirt_volume.cm-database-qcow2.id}"
-#   }
-#
-#   graphics {
-#     type = "spice"
-#     listen_type = "address"
-#     autoport = true
-#   }
-# }
-#
-# resource "libvirt_domain" "cm-backend" {
-#   name   = "${var.server_backend["hostname"]}"
-#   memory = "${var.server_backend["memory"]}"
-#   vcpu   = "${var.server_backend["vcpu"]}"
-#
-#   cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
-#
-#   network_interface {
-#     network_id = "${libvirt_network.vm_network.id}"
-#
-#     hostname  = "${var.server_backend["hostname"]}"
-#     addresses = [ "${var.server_backend["ip"]}" ]
-#     wait_for_lease = 1
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_port = "0"
-#     target_type = "serial"
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_type = "virtio"
-#     target_port = "1"
-#   }
-#
-#   disk {
-#     volume_id = "${libvirt_volume.cm-backend-qcow2.id}"
-#   }
-#
-#   graphics {
-#     type = "spice"
-#     listen_type = "address"
-#     autoport = true
-#   }
-# }
-#
-# resource "libvirt_domain" "cm-frontend" {
-#   name   = "${var.server_frontend["hostname"]}"
-#   memory = "${var.server_frontend["memory"]}"
-#   vcpu   = "${var.server_frontend["vcpu"]}"
-#
-#   cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
-#
-#   network_interface {
-#     network_id = "${libvirt_network.vm_network.id}"
-#
-#     hostname = "cm-frontend"
-#     hostname  = "${var.server_frontend["hostname"]}"
-#     addresses = [ "${var.server_frontend["ip"]}" ]
-#     wait_for_lease = 1
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_port = "0"
-#     target_type = "serial"
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_type = "virtio"
-#     target_port = "1"
-#   }
-#
-#   disk {
-#     volume_id = "${libvirt_volume.cm-frontend-qcow2.id}"
-#   }
-#
-#   graphics {
-#     type = "spice"
-#     listen_type = "address"
-#     autoport = true
-#   }
-# }
-#
-# resource "libvirt_domain" "cm-delivery" {
-#   name   = "${var.server_delivery["hostname"]}"
-#   memory = "${var.server_delivery["memory"]}"
-#   vcpu   = "${var.server_delivery["vcpu"]}"
-#
-#   cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
-#
-#   network_interface {
-#     network_id = "${libvirt_network.vm_network.id}"
-#
-#     hostname  = "${var.server_delivery["hostname"]}"
-#     addresses = [ "${var.server_delivery["ip"]}" ]
-#     wait_for_lease = 1
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_port = "0"
-#     target_type = "serial"
-#   }
-#
-#   console {
-#     type        = "pty"
-#     target_type = "virtio"
-#     target_port = "1"
-#   }
-#
-#   disk {
-#     volume_id = "${libvirt_volume.cm-delivery-qcow2.id}"
-#   }
-#
-#   graphics {
-#     type = "spice"
-#     listen_type = "address"
-#     autoport = true
-#   }
-# }
-#
+resource "libvirt_domain" "cm-database" {
+  name   = "${var.server_database["hostname"]}"
+  memory = "${var.server_database["memory"]}"
+  vcpu   = "${var.server_database["vcpu"]}"
+
+  cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
+
+  network_interface {
+    network_id = "${libvirt_network.vm_network.id}"
+
+    hostname  = "${var.server_database["hostname"]}"
+    addresses = [ "${var.server_database["ip"]}" ]
+    wait_for_lease = 1
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.cm-database-qcow2.id}"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+resource "libvirt_domain" "cm-backend" {
+  name   = "${var.server_backend["hostname"]}"
+  memory = "${var.server_backend["memory"]}"
+  vcpu   = "${var.server_backend["vcpu"]}"
+
+  cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
+
+  network_interface {
+    network_id = "${libvirt_network.vm_network.id}"
+
+    hostname  = "${var.server_backend["hostname"]}"
+    addresses = [ "${var.server_backend["ip"]}" ]
+    wait_for_lease = 1
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.cm-backend-qcow2.id}"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+resource "libvirt_domain" "cm-frontend" {
+  name   = "${var.server_frontend["hostname"]}"
+  memory = "${var.server_frontend["memory"]}"
+  vcpu   = "${var.server_frontend["vcpu"]}"
+
+  cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
+
+  network_interface {
+    network_id = "${libvirt_network.vm_network.id}"
+
+    hostname = "cm-frontend"
+    hostname  = "${var.server_frontend["hostname"]}"
+    addresses = [ "${var.server_frontend["ip"]}" ]
+    wait_for_lease = 1
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.cm-frontend-qcow2.id}"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+resource "libvirt_domain" "cm-delivery" {
+  name   = "${var.server_delivery["hostname"]}"
+  memory = "${var.server_delivery["memory"]}"
+  vcpu   = "${var.server_delivery["vcpu"]}"
+
+  cloudinit = "${libvirt_cloudinit_disk.commoninit_centos.id}"
+
+  network_interface {
+    network_id = "${libvirt_network.vm_network.id}"
+
+    hostname  = "${var.server_delivery["hostname"]}"
+    addresses = [ "${var.server_delivery["ip"]}" ]
+    wait_for_lease = 1
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.cm-delivery-qcow2.id}"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
